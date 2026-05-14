98b9dc193d173326b7a43ec518a7f09bdcbb30da
akryeziu <akryeziu@3cis.net>
2026-05-14 13:22:25 +0200

    feat: Architecting Project        - Creating projects        - Reorganize Files        - Category services interface and implement  category service        - Consume category service        - Adding Areas to project and adding layout to areas

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,4 +1,6 @@
-using BulkyBookWeb.Data;
+using BulkyBook.Business.Services;
+using BulkyBook.Business.Services.IServices;
+using BulkyBook.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SQLConnection"))
     );
+
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 var app = builder.Build();
 
@@ -27,9 +31,16 @@ app.UseAuthorization();
 
 app.MapStaticAssets();
 
+
+app.MapControllerRoute(
+    name: "MyArea",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Home}/{action=Index}/{id?}",
+    defaults: new { area = "Customer" })
     .WithStaticAssets();
 
 
