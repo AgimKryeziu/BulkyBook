c079a32d2ba46ca926fcee5d95849fdd3a369346
akryeziu <akryeziu@3cis.net>
2026-05-15 12:12:18 +0200

    feat: Home and Details        - Home UI        - Home page in action        - Load Details UI        - Details page in action

--- BulkyBook.Business/Services/ProductService.cs
+++ BulkyBook.Business/Services/ProductService.cs
@@ -21,15 +21,24 @@ namespace BulkyBook.Business.Services
             if (includeCategory)
             {
                 return await _context.Products.Include(u => u.Category).ToListAsync();
-            } else
+            }
+            else
             {
                 return await _context.Products.ToListAsync();
             }
         }
 
-        public async Task<Product?> GetProductByIdAsync(int id)
+        public async Task<Product?> GetProductByIdAsync(int id, bool includeCategory = false)
         {
-            return await _context.Products.FindAsync(id);
+            if (includeCategory)
+            {
+                return await _context.Products.Include(u => u.Category).FirstOrDefaultAsync(u => u.Id == id);
+            }
+            else
+            {
+                return await _context.Products.FirstOrDefaultAsync(u => u.Id == id);
+            }
+
         }
 
 
