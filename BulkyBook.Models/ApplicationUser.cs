aa5072dd4f8b92e85b2c83327b237aaa72e0c045
akryeziu <akryeziu@3cis.net>
2026-05-19 15:07:13 +0200

    feat: Mega assignment - Users        - User list page        - Lock and unlock user        - Lochout in action        - Role managment UI        - Make role Managment UI dynamic        - Update role i action        - Change password action method

--- BulkyBook.Models/ApplicationUser.cs
+++ BulkyBook.Models/ApplicationUser.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace BulkyBook.Models
@@ -11,11 +12,12 @@ namespace BulkyBook.Models
         [Required]
         public string Name { get; set; } = string.Empty;
 
-        public string? StreetAddress { get; set; } 
-        public string? City { get; set; } 
-        public string? State { get; set; } 
-        public string? PostalCode { get; set; } 
+        public string? StreetAddress { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
 
-
+        [NotMapped]
+        public string Role { get; set; }
     }
 }
--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
     options.Password.RequireNonAlphanumeric = true;             // Must have at least one special character (!@#$%^&*)
     options.Password.RequiredLength = 6;                        // Minimum 6 characters
     options.Password.RequiredUniqueChars = 1;                   // Minimum unique characters
-}).AddEntityFrameworkStores<ApplicationDbContext>();
+}).AddDefaultTokenProviders().AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IProductService, ProductService>();
